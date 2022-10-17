1271d2b6aa5474e4ecdbe9243231fda315b94a7e
Óscar Rivas <oscar.rivas.99@gmail.com>
2022-10-18 00:00:01 +0200

    first strep for tipography simulator

--- src/pages/SimulacionInteractiva.jsx
+++ src/pages/SimulacionInteractiva.jsx
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import ColorContrast from "../components/ColorContrast";
 
+import imgB from "../img/simulations/1.png";
+import imgA from "../img/simulations/2.png";
+
 export default function SimulacionInteractiva() {
+  const [pos, setPos] = useState(50);
+
   return (
-    <div>
-      <ColorContrast />
-    </div>
+    <>
+      <div className="oscaryosoy">
+        <div className="background-img text-end">
+          <p className="position-absolute end-0">adios</p>
+          <img src={imgB} alt="" />
+        </div>
+        <div className="foreground-img overflow-hidden" style={{ width: `${pos}%` }}>
+          <p className="position-absolute">jola</p>
+          <img src={imgA} alt="" />
+        </div>
+        <input
+          type="range"
+          min="1"
+          max="100"
+          value={pos}
+          className="slider"
+          name="slider"
+          id="slider"
+          onChange={(e) => {
+            setPos(e.target.value);
+          }}
+        />
+        <div className="slider-button cursor-pointer" style={{ left: `calc(${pos}% - 18px)` }}></div>
+      </div>
+      <div>
+        <ColorContrast />
+      </div>
+    </>
   );
 }
