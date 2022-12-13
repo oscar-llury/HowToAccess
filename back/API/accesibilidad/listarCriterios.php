72747e2049a895c771c46ddb480c17d5a45518d1
Óscar Rivas <oscar.rivas.99@gmail.com>
2022-12-13 20:40:36 +0100

    fix bug criteria unordered in create new project

--- back/API/accesibilidad/listarCriterios.php
+++ back/API/accesibilidad/listarCriterios.php
@@ -28,14 +28,14 @@ INNER JOIN acc_principio ON acc_criterio_conformidad.principio_id = acc_principi
 INNER JOIN acc_pauta ON acc_criterio_conformidad.pauta_id = acc_pauta.id AND acc_pauta.activo=1
 INNER JOIN acc_nivel_conformidad ON acc_criterio_conformidad.nivel_conformidad = acc_nivel_conformidad.id AND acc_nivel_conformidad.activo=1
 where acc_criterio_conformidad.activo=1';
-if($conformanceLevel){
+if ($conformanceLevel) {
     $query .= ' and acc_criterio_conformidad.nivel_conformidad IN(1';
-    for ($i = 2; ($i <= $conformanceLevel) && ($conformanceLevel >1) && ($conformanceLevel <=3); $i++){
-        $query.= ','.$i;
+    for ($i = 2; ($i <= $conformanceLevel) && ($conformanceLevel > 1) && ($conformanceLevel <= 3); $i++) {
+        $query .= ',' . $i;
     }
     $query .= ')';
 }
-
+$query .=  ' order by cod_principio';
 $allCriterios = $contextDB->query($query)->fetchAll();
 
 if ($allCriterios) {
@@ -45,42 +45,41 @@ if ($allCriterios) {
     $objRespuesta->data = array();
 
     $idPrincipio = 0;
-    $idPauta=0;
-    foreach($allCriterios as $record){
+    $idPauta = 0;
+    foreach ($allCriterios as $record) {
         //var_dump($prj);
         //var_dump($id);
         //var_dump($prj);
         //var_dump($prj);
         //echo '<br><br>';
-        if($record->cod_pauta != $idPauta){
+        if ($record->cod_pauta != $idPauta) {
             //cambiamos de pauta, guardar la anterior
-            if($idPauta > 0){
+            if ($idPauta > 0) {
                 //no es la primera iteracion
-                array_push($objPrincipio->pautas ,$objPauta);
+                array_push($objPrincipio->pautas, $objPauta);
             }
-            
+
             $objPauta = new StdClass();
-            if($idPauta == 0){
+            if ($idPauta == 0) {
                 //primera pauta de todas
-                $objPauta->key = '1.'.$record->cod_pauta;
-            }else if($record->cod_principio != $idPrincipio){
+                $objPauta->key = '1.' . $record->cod_pauta;
+            } else if ($record->cod_principio != $idPrincipio) {
                 //primera pauta de nuevo principio
-                $objPauta->key = $record->cod_principio.'.'.$record->cod_pauta;
-            }
-            else{
-                $objPauta->key = $objPrincipio->code.'.'.$record->cod_pauta;
+                $objPauta->key = $record->cod_principio . '.' . $record->cod_pauta;
+            } else {
+                $objPauta->key = $objPrincipio->code . '.' . $record->cod_pauta;
             }
             $idPauta = $record->cod_pauta;
             $objPauta->name = $record->nombre_pauta;
             $objPauta->code =  $record->cod_pauta;
             $objPauta->criterios = array();
         }
-        if($record->cod_principio != $idPrincipio){
+        if ($record->cod_principio != $idPrincipio) {
             //cambiamos de principio
-            if($idPrincipio > 0){
+            if ($idPrincipio > 0) {
                 //no es la primera iteracion
-                
-                array_push($objRespuesta->data ,$objPrincipio);
+
+                array_push($objRespuesta->data, $objPrincipio);
             }
             $objPrincipio = new StdClass();
             $idPrincipio = $record->cod_principio;
@@ -89,17 +88,17 @@ if ($allCriterios) {
             $objPrincipio->name = $record->nombre_principio;
             $objPrincipio->pautas = array();
         }
-        
+
         $objCriterio = new stdClass();
-        $objCriterio->key = $objPrincipio->code.'.'.$record->cod_pauta.'.'.$record->cod_criterio;
+        $objCriterio->key = $objPrincipio->code . '.' . $record->cod_pauta . '.' . $record->cod_criterio;
         $objCriterio->code = $record->cod_criterio;
         $objCriterio->name = $record->nombre_criterio;
         $objCriterio->level_code = $record->nivel_conformidad_id;
         $objCriterio->level_name = $record->nivel_conformidad_code;
-        array_push($objPauta->criterios ,$objCriterio);
+        array_push($objPauta->criterios, $objCriterio);
     }
-    array_push($objPrincipio->pautas ,$objPauta);
-    array_push($objRespuesta->data ,$objPrincipio);
+    array_push($objPrincipio->pautas, $objPauta);
+    array_push($objRespuesta->data, $objPrincipio);
 } else {
     $objRespuesta->status = 0;
     $objRespuesta->code = 2;
